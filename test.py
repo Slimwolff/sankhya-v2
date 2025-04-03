972766a3e171728bd0c38efb6969b26f5de200a2
Slimwolff <ptudoandtodos@gmail.com>
2025-04-03 17:30:20 -0300

    simple test some code in python

---
+++ test.py
@@ -0,0 +1,5 @@
+str = "Nota(s) com frete incluso.Não é possível vincular o CT-e com esta(s) nota(s) Nro(s) Únicos(s): 149692, 157279, 157694, 157695, 157696, 157706, 158233, 158231, 158302"
+
+x = str.split(":")
+
+print(x)
