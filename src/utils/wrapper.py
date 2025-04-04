286471dc1c851d98259b3a2089bd05338c930ee9
Slimwolff <ptudoandtodos@gmail.com>
2025-04-04 17:51:51 -0300

    feat: wrapper to use request using toml settings

--- src/utils/wrapper.py
+++ src/utils/wrapper.py
@@ -1,16 +1,7 @@
-import os
-import tomllib
-
-# Diretório base do projeto (assumindo que 'wrapper.py' está em 'src/utils/')
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Sobe para 'src/'
-
-print(BASE_DIR)
-
-# Caminho absoluto do arquivo TOML
-CONFIG_PATH = os.path.join(BASE_DIR, "config", "config.toml")
-
-# Abre o arquivo
-# with open(CONFIG_PATH, mode="rb") as fp:
-#     config = tomllib.load(fp)
-
-# print(config)
+from .load_config import getConfig
+from .snk_requester import Snk
+config = getConfig()
+wrapper = Snk(
+    f"http://{config['env']['dominio']}",
+    config['env']['browser']
+)
