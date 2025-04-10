f1ec5fd0677073b51ac4a8fa790e20bc103ddba9
Slimwolff <ptudoandtodos@gmail.com>
2025-04-10 17:31:37 -0300

    feat: getNuarquivo in new file

--- src/core/LancarCTEs.py
+++ src/core/LancarCTEs.py
@@ -8,75 +8,30 @@
 # renegoceia titulo com numero do documento informado
 import json
 import re
-from ..services.carregarRegistros import carregarRegistros
+
+from core.getNuarquivoFromNumnotas import getNuarquivoFromNumnotas
 from ..services.processarNotaArquivo import processarNotaArquivo
-
-def getNuarquivoFromNumnotas(nu: list):
-
-    n = ",".join(str(element) for element in nu)
-
-    print(f"numnotas: {n}")
-
-    records = carregarRegistros(
-        entityName="ImportacaoXMLNotas",
-        crudListener="br.com.sankhya.modelcore.comercial.ImportacaoXmlNotasCrudListener",
-        orderByExpression="NUARQUIVO",
-        fields=[
-            "NUARQUIVO",
-            "DIASEMISSAOCALC",
-            "TipoOperacao.DESCROPER",
-            "CODEMP",
-            "Empresa.NOMEFANTASIA",
-            "CODPARC",
-            "Parceiro.NOMEPARC",
-            "CODVEND",
-            "SITUACAOMDE",
-            "IMPORTADOMDE",
-            "SITUACAONFE",
-            "TEMXML",
-            "VLRNOTA",
-            "DHEMISS",
-            "TIPO",
-            # "CONFIG",
-            "DHPROCESS",
-            # "XML",
-            "DHIMPORT",
-            "NUMNOTA",
-            # "NUARQUIVO",
-            "CHAVEACESSO",
-            "DETALHESIMPORTACAO",
-            "NOMEARQUIVO",
-            "STATUS",
-            "NUNOTA",
-            "NUFIN",
-            "TIPIMPCTE",
-        ],
-        expression=f"(onlydate(this.DHEMISS) >= {'01/03/2025'}) AND ((ImportacaoXMLNotas.NUMNOTA IN ({n}) ))"
-    )
-    print(f"getNuArquivoFromNumnotas -> records{records}")
-    arr = records['responseBody']['result']
-    nuArquivo = []
-    for n in arr:
-        nuArquivo.append(n[0])
-    return nuArquivo
-    
 
 # getNuarquivoFromNumnotas(6357523,6366813)
 
 def launchCTE(numNotas: list):
-    nuArquivos = getNuarquivoFromNumnotas(numNotas)
+    try:
+        nuArquivos = getNuarquivoFromNumnotas(numNotas)
+        holding = processarNotaArquivo(nuArquivos)
 
-    holding = processarNotaArquivo(nuArquivos)
+        processedNotes = holding['responseBody']['avisos']['aviso']
 
-    processedNotes = holding['responseBody']['avisos']['aviso']
+        diverNotes = []
 
-    diverNotes = []
-
-    for n in processedNotes:
-        if re.search("Divergência",n["$"]) is not None:
-            match = re.search(r'Arquivo:\s*(\d+)',n["$"])
-            if match:
-                diverNotes.append(match.group(1))
-    print(diverNotes)
+        for n in processedNotes:
+            if re.search("Divergência",n["$"]) is not None:
+                match = re.search(r'Arquivo:\s*(\d+)',n["$"])
+                if match:
+                    diverNotes.append(match.group(1))
+        print(diverNotes)
+    except Exception as e:
+        print(f"Erro: {e}")
+        
+    
 
 launchCTE([6332819,6334510,6337246])
