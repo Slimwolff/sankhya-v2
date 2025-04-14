5838d2e8eb7c7457bf1041a109c446b740a0c90f
Slimwolff <ptudoandtodos@gmail.com>
2025-04-14 17:31:27 -0300

    feat: teste concluido com regex search

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-str = "Nota(s) com frete incluso.Não é possível vincular o CT-e com esta(s) nota(s) Nro(s) Únicos(s): 149692, 157279, 157694, 157695, 157696, 157706, 158233, 158231, 158302"
+import re
 
-x = str.split(":")
+s = "Nota(s) com frete incluso.Não é possível vincular o CT-e com esta(s) nota(s) Nro(s) Únicos(s): 149692, 157279, 157694, 157695, 157696, 157706, 158233, 158231, 158302"
 
-print(x)
+x = re.search(r":\s*7[0-9]", s)
+
+print(x.group())
