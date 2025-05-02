fff48e654fae9edd2685863713c0d189b24fc6e8
Slimwolff <ptudoandtodos@gmail.com>
2025-05-02 18:01:50 -0300

    feat: new confirmarNota method

---
+++ src/services/ConfirmarNota.py
@@ -0,0 +1,30 @@
+from typing import Dict, Any
+from utils.wrapper import wrapper
+
+def confirmarNota(nroUnico: int | str) -> Dict[str, Any]:
+    """Retorna respota da confirmação da Nota
+
+    Args:
+        nroUnico (int | str): _description_
+
+    Returns:
+        Dict[str, Any]: _description_
+    """
+    r= wrapper.request("CACSP.confirmarNota",requestBody={
+        "nota": {
+            "confirmacaoCentralNota": "true",
+            "ehPedidoWeb": "false",
+            "atualizaPrecoItemPedCompra": "false",
+            "ownerServiceCall": "CentralNotas_CentralNotas_0",
+            "visAutOcorrencias": "true",
+            "NUNOTA": {
+                "$": nroUnico
+            }
+        }
+    },
+    cookie="mgecom"
+    )
+    print(r)
+    return r
+
+confirmarNota(170289)
